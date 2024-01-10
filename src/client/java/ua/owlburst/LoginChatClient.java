73251fb900d166fd2fcffcd041796d1d27e1bdde
whitebear60 <owlburst@outlook.com>
2024-01-10 16:33:52 +0200

    Clean up the redundant common code

--- src/client/java/ua/owlburst/LoginChatClient.java
+++ src/client/java/ua/owlburst/LoginChatClient.java
@@ -5,26 +5,19 @@ import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.fabricmc.fabric.api.networking.v1.PacketSender;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
-import net.minecraft.text.Text;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class LoginChatClient implements ClientModInitializer {
+	public static final Logger LOGGER = LoggerFactory.getLogger("loginchat");
 	private static void onPlayReady(ClientPlayNetworkHandler handler, PacketSender sender, MinecraftClient client) {
 		if (client.player != null) {
 //			client.player.sendCommand("say Hello, BronyTales!");
+			client.player.sendCommand("/mcability");
 			client.player.sendChatMessage("Hello everypony!", null);
 			if (client.getCurrentServerEntry() != null) {
-				LoginChat.LOGGER.info(String.valueOf(client.isInSingleplayer()));
-				LoginChat.LOGGER.info(client.getCurrentServerEntry().address);
-			}
-		}
-	}
-	private static void onPlayDisconnect(ClientPlayNetworkHandler handler, MinecraftClient client) {
-		if (client.player != null) {
-			client.player.sendCommand("/mcability");
-			client.player.sendChatMessage("Goodbye everypony!", null);
-			if (client.getCurrentServerEntry() != null) {
-				LoginChat.LOGGER.info(String.valueOf(client.isInSingleplayer()));
-				LoginChat.LOGGER.info(client.getCurrentServerEntry().address);
+				LOGGER.info(String.valueOf(client.isInSingleplayer()));
+				LOGGER.info(client.getCurrentServerEntry().address);
 			}
 		}
 	}
@@ -32,6 +25,5 @@ public class LoginChatClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		ClientPlayConnectionEvents.JOIN.register((LoginChatClient::onPlayReady));
-		ClientPlayConnectionEvents.DISCONNECT.register(LoginChatClient::onPlayDisconnect);
 	}
 }
